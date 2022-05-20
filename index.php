7c8b3e3cf862ce6d1c0cd038c05f82d79c73886f
GiulianoCretella <giulianocretella.1@gmail.com>
2022-05-20 13:58:49 +0200

    errore battitura corretto

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$title="Non sarebbe stato più facile continuare non vue.js? 'e grazie al php'";
+$title="Non sarebbe stato più facile continuare con vue.js? 'e grazie al php'";
 $censured=$_GET['censured'];
 $censure_placed_string= str_replace($censured,'***', $title);
 ?>
@@ -13,7 +13,7 @@ $censure_placed_string= str_replace($censured,'***', $title);
     <title>Document</title>
 </head>
 <body>
-    <p>Per censurare una parola usare "?censured=(parola da censurare)" nella barra url <br> P.S. $liberaScelta = 'libera interpretazione'</p>
+    <p>Per censurare una parola usare : "?censured=(parola da censurare)" nella barra url <br> P.S. $liberaScelta = 'libera interpretazione'</p>
     <h1><?php echo $title?></h1>
     <p><?php echo strlen($title)?></p>
 
